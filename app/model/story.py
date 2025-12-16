f2521c89366325ae1678c794ebf7598d4cc94e3a
Lucien Ramond <lucien_ramond@hotmail.com>
2025-12-17 00:18:35 +0100

    Add creator to story

--- app/model/story.py
+++ app/model/story.py
@@ -4,6 +4,7 @@ class Story(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     story = db.Column(db.String(10000), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return str(self.id)
--- app/services/stories_service.py
+++ app/services/stories_service.py
@@ -1,23 +1,26 @@
 from model.story import Story
+from model.user import User
 from server import db
+from sqlalchemy.orm import contains_eager, joinedload
 
 class StoriesService():
         def get_all_stories():
 
             try:
-                  stories = (Story.query.all())
+                  stories = Story.query.all()
+                  users = User.query.all()
             except Exception as e:
                   return (f'{e}')
-
 
             return [{
                 "id":story.id,
                 "name":story.name,
                 "story":story.story,
+                "created_by":[{"name": user.username, "id": user.id} for user in users if user.id == story.created_by]
             } for story in stories]
 
         def create_story(story_data):
-            story = Story(name=story_data["name"], story=story_data['story'])
+            story = Story(name=story_data["name"], story=story_data['story'], created_by=story_data['user_id'])
             db.session.add(story)
             db.session.commit()
 
