529015e3272434b2dd3db90840d26e4fc8b0150e
Lucien Ramond <lucien_ramond@hotmail.com>
2025-12-30 14:45:04 +0100

    Update user model

--- app/model/user.py
+++ app/model/user.py
@@ -2,8 +2,8 @@ from server import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    avatar_img = db.Column(db.String(255), unique=True, nullable=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    avatar_img = db.Column(db.String(255), nullable=True)
     password_hash = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
